c3cd8aa9bd6ea8709b38a3d16245523ddabf763e
paul van bladel <paul.vanbladel@gmail.com>
2017-03-24 19:06:37 +0100

    comment in Hello spec

--- test/unit/specs/Hello.spec.ts
+++ test/unit/specs/Hello.spec.ts
@@ -4,6 +4,8 @@ import Hello from '../../../src/components/Hello.vue';
 import { expect } from 'chai';
 describe('Hello.vue', () => {
   it('should render correct contents', () => {
+    // surprisingly this test needs to be setup with a template and components param in the vue extend method, WHY??????
+
     const Constructor = Vue.extend({ template: '<div><hello></hello></div>', components: { 'hello': Hello } });
     const vm = new Constructor().$mount();
     expect(vm.$el.querySelector('.hello h1').textContent)
