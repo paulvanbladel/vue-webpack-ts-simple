9c74f2631bc6d2f91080f6fc8ee84b06dfeb5ae0
paul van bladel <paul.vanbladel@gmail.com>
2017-03-25 09:47:34 +0100

    unit test for showing the vue.extend problem with vue-class-component

--- test/unit/specs/Bonjour.spec.ts
+++ test/unit/specs/Bonjour.spec.ts
@@ -4,7 +4,9 @@ import { expect } from 'chai';
 describe('Bonjour.vue', () => {
   it('should render correct contents', () => {
     debugger;
+    // this is the standard out of the box experience for setting up a vm under test
     const Constructor = Vue.extend(Bonjour);
+
     const vm = new Constructor().$mount();
     expect(vm.$el.querySelector('.hello h1').textContent)
       .to.equal('I am a real dump component without any logic just some data');
--- test/unit/specs/Hello.spec.ts
+++ test/unit/specs/Hello.spec.ts
@@ -5,14 +5,19 @@ import { expect } from 'chai';
 describe('Hello.vue', () => {
   it('should render correct contents', () => {
     // surprisingly this test needs to be setup with a template and components param in the vue extend method, WHY??????
-    debugger;
-    const Constructor = Vue.extend({ template: '<div><hello></hello></div>', components: { 'hello': Hello } });
+    // when running th s test with  following 2 lines it will BREAK:
+    const Constructor = Vue.extend(Hello);
+
     const vm = new Constructor().$mount();
+
+    // we can only fix this test by using following 2 lines: 
+    //  const Constructor = Vue.extend({ template: '<div><hello></hello></div>', components: { 'hello': Hello } });
+    //  const vm = new Constructor().$mount();
     expect(vm.$el.querySelector('.hello h1').textContent)
       .to.equal('Welcome to Your Vue.js App');
     let resultFromMyMethod = (<any>vm.$children[0]).mymethod();
     expect(resultFromMyMethod).to.equal('ok');
-    
+
   });
 });
 
